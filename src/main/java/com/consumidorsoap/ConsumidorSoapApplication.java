cf3c326a055584c7ed019d13353da8efc63b6784
Vanderlei Loureiro <vanderloureiro.dev@gmail.com>
2021-11-13 19:44:40 -0300

    primeiro commit com dependencias

---
+++ src/main/java/com/consumidorsoap/ConsumidorSoapApplication.java
@@ -0,0 +1,13 @@
+package com.consumidorsoap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumidorSoapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumidorSoapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/consumidorsoap/ConsumidorSoapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.consumidorsoap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumidorSoapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
